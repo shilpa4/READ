c4fd2679c48cfad514b88dcef579e16d1a071bfd
shilpa4 <shilpasre91@gmail.com>
2016-08-30 14:00:08 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,28 @@
+package guvi;
+
+public class reverse {
+	public static void main(String [ ] arg){
+	String s="i love india",out="";
+	String a[]=s.split(" ");
+	for(int i=a.length-1;i>=0;i--){
+		
+		out=out+a[i]+" ";
+	}
+	
+	System.out.println(out);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
