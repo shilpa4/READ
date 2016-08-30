51c96ad00885adb9a7fea5acbd43041a388cc90b
shilpa4 <shilpasre91@gmail.com>
2016-08-30 14:21:04 +0530

    Add files via upload

---
+++ median.java
@@ -0,0 +1,22 @@
+package guvi;
+
+public class median {
+	public static void main(String [ ] arg){
+String s="computer",out="";
+StringBuffer ff=new StringBuffer(s);
+ff.reverse();
+String dd=new String(ff);
+for(int i=0;i<dd.length();i++){
+out=out+dd.charAt(i)+"-";
+		
+}
+System.out.println(out);
+		
+		
+		
+		
+		
+		
+		
+	}
+}
