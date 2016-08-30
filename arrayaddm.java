3868fe77fbff4da787304b9205915081ff12276d
shilpa4 <shilpasre91@gmail.com>
2016-08-30 15:17:36 +0530

    Add files via upload

---
+++ arrayaddm.java
@@ -0,0 +1,34 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class arrayaddm {
+	public static void main(String [ ] arg){
+
+	int a[]={4,5,2,1};
+	int m=5;
+	int target=32,sum=0;
+	for(int i=0;i<a.length;i++){
+		sum=sum+(a[i]+m);
+	}
+
+if(sum==target){
+	System.out.println("true");
+}else{
+	System.out.println("false");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
