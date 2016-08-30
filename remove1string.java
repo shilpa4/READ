ac91ab70d209fa2fad23d4875534cab0442f373b
shilpa4 <shilpasre91@gmail.com>
2016-08-30 11:35:24 +0530

    Add files via upload

---
+++ remove1string.java
@@ -0,0 +1,10 @@
+package guvi;
+
+public class remove1string {
+	public static void main(String arg[]){
+	String mask="in";
+	String input="beingibbbing";
+	String sa=input.replaceAll(mask,"");
+	System.out.println(sa);
+	}
+}
