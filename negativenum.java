1f9399e5054b226f85b1d1894e42681ad2a56e44
shilpa4 <shilpasre91@gmail.com>
2016-08-30 12:20:36 +0530

    Add files via upload

---
+++ negativenum.java
@@ -0,0 +1,19 @@
+package guvi;
+
+public class negativenum {
+	public static void main(String arg[]){
+String s="-123";
+try{
+int a=Integer.parseInt(s);
+	if(a<0){
+		System.out.println(a);
+
+	}
+}
+catch(Exception e){
+System.out.println("-1");}
+	
+	
+	
+	}
+}
